683678a71f2921ae2839751dff39260fe020fec8
yjm29yjm <112875739+yjm29yjm@users.noreply.github.com>
2022-09-09 19:17:51 +0800

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int n = 0;
+	int ret = 1;
+	scanf_s("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		ret = ret * i;
+	}
+	printf("ret = %d", ret);
+
+
+
+
+
+	return 0;
+}
