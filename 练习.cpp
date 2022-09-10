9e5095b86338e333cfc93205fd4b44519dcfd11d
yjm29yjm <112875739+yjm29yjm@users.noreply.github.com>
2022-09-10 16:29:48 +0800

    Add files via upload

---
+++ 练习.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while(left<=right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
+
+	}
+
+	return 0;
+
+}
