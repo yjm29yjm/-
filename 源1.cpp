626ee8a7bb0dd338e46d97cbcab46c5ba02cd448
yjm29yjm <112875739+yjm29yjm@users.noreply.github.com>
2022-09-16 07:43:10 +0800

    Add files via upload

--- 源1.cpp
+++ 源1.cpp
@@ -1,25 +1,29 @@
 #include <stdio.h>
 int main()
 {
-	int day = 0;
-	scanf_s("%d", &day);
-	switch (day)
+	int count = 0;
+	int num = 0;
+	int i = 1;
+	int a[19];
+
+	for (i = 0; i <20; i++)
 	{
-	case 1:
-			printf("星期一\n");
-	case 2:
-			printf("星期二\n");
-	case 3:
-				printf("星期三\n");
-				break;
+		scanf_s("%d", &a[i]);
 
+		if (a[i] % 2 == 0)
+		{
+			count++;
+		}
 
-
-
+		else
+		{
+			num++;
+		}
+		if(i==20)
+		break;
 	}
-
-	
-
+	printf("%d\n", count);
+	printf("%d", num);
 	return 0;
 
 }
