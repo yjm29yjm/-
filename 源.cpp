d4df694b7035e21bd036506dada2dff0c103c9c6
yjm29yjm <112875739+yjm29yjm@users.noreply.github.com>
2022-09-17 21:01:58 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,8 +1,37 @@
 #include <stdio.h>
+
 int main()
 {
-	int A, B;
-	scanf_s("%d %d", &A, &B);
-	printf("%d",A+B);
+	int t;
+	while (scanf_s("%d", &t))
+	{
+		if( (t < 60)&&(t>=0))
+		
+			printf("E\n");
+		
+
+		if ((t > 59) && (t < 70))
+		
+			printf("D\n");
+		
+		if ((t > 69) && (t < 80))
+		
+			printf("C\n");
+		
+		if ((t > 79) && (t < 90))
+		
+			printf("B\n");
+		
+		if ((t > 89) && (t < 101))
+		
+			printf("A\n");
+		
+		if ((t > 100)||(t<0))
+		
+			printf("Score is error!");
+			break;
+		
+	}
+
 	return 0;
 }
