defb590d5a2beeb7f5e11b894b94ea35a38d5bdb
yjm29yjm <112875739+yjm29yjm@users.noreply.github.com>
2022-09-22 16:51:17 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,37 +1,17 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-	int t;
-	while (scanf_s("%d", &t))
+	int i, n, x, j;//定义循环变量i，循环次数n和未知数x以及乘积j
+	while (scanf_s("%d", &n) != EOF)//当输入未强制终止
 	{
-		if( (t < 60)&&(t>=0))
-		
-			printf("E\n");
-		
-
-		if ((t > 59) && (t < 70))
-		
-			printf("D\n");
-		
-		if ((t > 69) && (t < 80))
-		
-			printf("C\n");
-		
-		if ((t > 79) && (t < 90))
-		
-			printf("B\n");
-		
-		if ((t > 89) && (t < 101))
-		
-			printf("A\n");
-		
-		if ((t > 100)||(t<0))
-		
-			printf("Score is error!");
-			break;
-		
+		j = 1;//初始化j的结果
+		for (i = 1; i <= n; i++)//循环n次，每次输入一个x
+		{
+			scanf_s("%d", &x);//输入x，开始进行判定
+			if (x % 2 == 1 || x % 2 == -1)//如果是奇数
+				j *= x;//累乘
+		}
+		printf("%d\n", j);//输出乘积的结果
 	}
-
-	return 0;
+	return 0;//程序正常结束
 }
